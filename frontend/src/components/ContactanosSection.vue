9b259c9b5b3efa26bb57c7f7678ff56473776014
silvermiguel96 <silvermiguel96@gmail.com>
2021-07-26 20:08:10 -0500

    Contact Section

--- frontend/src/components/ContactanosSection.vue
+++ frontend/src/components/ContactanosSection.vue
@@ -1,52 +1,34 @@
 <template>
-  <section id="contactanos">
-    <v-container fluid>
+  <section id="contactanos" class="py-3">
+    <v-container class="d-flex">
       <v-row align="center" justify="center">
-        <v-col cols="10">
-          <v-row align="center" justify="center">
-            <v-col sm="5" class="hidden-xs-only">
-              <h1 class="font-weight-light display-2 mb-2">CONECTÉMONOS</h1>
-              <h1 class="font-weight-light">
-                Únete a nuestros canales y conoce todo lo que sucede en la
-                comunidad..
-              </h1>
-            </v-col>
-            <v-col cols="1" sm="5" class="">
-              <v-col cols="9">
-                <v-subheader
-                  ><v-icon class="mr-2"> mdi-check-circle </v-icon>
-                  Eventos</v-subheader
-                >
-                <v-subheader
-                  ><v-icon class="mr-2"> mdi-check-circle </v-icon>
-                  Charlas</v-subheader
-                >
-                <v-subheader
-                  ><v-icon class="mr-2"> mdi-check-circle </v-icon> Ofertas
-                  laborales</v-subheader
-                >
-                <v-subheader
-                  ><v-icon class="mr-2"> mdi-check-circle </v-icon> Noticias
-                  Tech</v-subheader
-                >
-                <v-subheader
-                  ><v-icon class="mr-2"> mdi-check-circle </v-icon> Hablar con
-                  otros desarrolladores</v-subheader
-                >
-                <v-subheader
-                  ><v-icon class="mr-2"> mdi-check-circle </v-icon>
-                  Networking</v-subheader
-                >
-              </v-col>
-            </v-col>
-          </v-row>
+        <v-col cols="12" md="6">
+          <h3 class="font-weight-light display-2 mb-2">CONECTÉMONOS</h3>
+          <p class="font-weight-light body-1">
+            Únete a nuestros canales y conoce todo lo que sucede en la
+            comunidad.
+          </p>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-subheader
+            class="body-1"
+            v-for="(service, index) in services"
+            :key="index"
+          >
+            <v-icon class="mx-3" large color="white">
+              mdi-check-circle
+            </v-icon>
+            {{ service.name }}</v-subheader
+          >
+        </v-col>
+        <v-col cols="12" class="d-flex justify-center">
           <v-btn
             rounded
             href=""
             target="_blank"
             large
             color="white"
-            class="mt-4"
+            class="mx-4"
           >
             <v-icon class="mr-2"> mdi-meetup </v-icon>
             Meetup
@@ -57,7 +39,7 @@
             target="_blank"
             large
             color="white"
-            class="mt-4"
+            class="mx-4"
           >
             <v-icon class="mr-2"> mdi-slack </v-icon>
             Slack
@@ -68,18 +50,25 @@
   </section>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      services: [
+        { name: "Eventos" },
+        { name: "Charlas" },
+        { name: "Ofertas laborales" },
+        { name: "Noticias Tech" },
+        { name: "Hablar con otros desarrolladores" },
+        { name: "Networking" },
+      ],
+    };
+  },
+};
+</script>
+
 <style scoped>
 #contactanos {
   background-color: slategray;
-  background-attachment: fixed;
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  height: 500px;
-}
-
-#contactanos .container,
-#contactanos .row {
-  height: 100%;
 }
 </style>
